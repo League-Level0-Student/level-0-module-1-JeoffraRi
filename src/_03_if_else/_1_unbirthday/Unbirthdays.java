d1c807ffb1f2e3e664c3efa0e01b2f0b18ab500c
League Student <league@iMac-12.attlocal.net>
2021-12-19 16:36:17 -0800

    Module1 Level 0
    the league of amazing prog.

---
+++ src/_03_if_else/_1_unbirthday/Unbirthdays.java
@@ -0,0 +1,18 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthdays {
+	public static void main(String[] args) {
+		String answer = JOptionPane.showInputDialog(null, "What is your birthday? (mm/dd)");
+		// 4. If they typed in today's date:
+	if(answer.equals("12/19")) {
+		JOptionPane.showMessageDialog(null,"Happy birthday! *horn sounds*");
+	}else{
+		JOptionPane.showMessageDialog(null,"A very merry unbirthday to u");
+	}
+	}
+	}
+	
+
+
